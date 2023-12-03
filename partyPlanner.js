0d8cef290b9f2fa659c5e9718cdf2f02c9cda41d
Sjpandya0210 <Sjpandya1991@gmail.com>
2023-12-03 01:10:49 -0500

    created delete function and tested all of the codes

--- partyPlanner.js
+++ partyPlanner.js
@@ -1,33 +1,38 @@
-const COHORT = "REPLACE_ME!";
-const API_URL= `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-pt-sf-b/events`
-console.log(API_URL);
+const COHORT = "2310-fsa-et-web-pt-sf-b-shruti";
+const apiUrl= `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`
+console.log(apiUrl);
 
 const state = {
-    parties: [],
+    partyEvents: [],
   };
+
   const partyList = document.querySelector("#parties");
-  const addPartyForm = document.querySelector("#addParty");
-  addPartyForm.addEventListener("submit", addParty);
+  const addPartyToForm = document.querySelector("#addParty");
+ 
+  addPartyToForm.addEventListener("submit", addParty);
+ 
 
 /**
  * Sync state with the API and rerender
  */
 async function render() {
-    await getParties();
-    renderParties();
+    await getParties(); //again and again calling the function 
+    renderParties(); // everytime we see if the party is in the event or need to create one.
   }
   render();
 
+
 /**
- * Update state with artists from API
+ * Update state with party from API
+ * function to get data from the server 
  */
 async function getParties() {
     try {
-      const response = await fetch(API_URL);
+      const response = await fetch(apiUrl);
       const jsonResponse = await response.json();
       console.log(jsonResponse); //to check what we have
-      const parties = jsonResponse.data;
-      state.parties = parties;
+      const partyEvents = jsonResponse.data; // because we only need data not errors and others
+      state.partyEvents = partyEvents;
       console.log('Parties:', state) //to test that its working
       
     } catch (error) {
@@ -37,51 +42,56 @@ async function getParties() {
 
 /**
  * Render party from state
+ * function to update the DOM with the list of 
  */
 function renderParties() {
-    if (!state.parties.length) {
+    if (!state.partyEvents.length) {
       partyList.innerHTML = "<li>No party.</li>";
       return;
     }
     // to create an artist, if we dont hv it in the list 
-    const partyCards = state.parties.map((party) => {
-      const button = document.createElement("button");
-      button.innerText = "Delete"
+    const partyCards = state.partyEvents.map((party) => {
       const li = document.createElement("li");
       li.innerHTML = `
         <h2>${party.name}</h2>
         <p>${party.date}</p>
-        <p>${party.time}</p>
         <p>${party.location}</p>
         <p>${party.description}</p>
-
-      `;
+        <button class="delete-party" data-party-id="${party.id}">X</button>
+    `;
       return li;
     });
   
     partyList.replaceChildren(...partyCards);
   }
 /**
- * Ask the API to create a new artist based on form data
+ * Ask the API to create a new party based on form data
  */
 async function addParty(event) {
     event.preventDefault();
   
     try {
-      const response = await fetch(API_URL, {
+      const currentDate = new Date();
+      const selectedDate = new Date(addPartyToForm.date.value);
+    // Check if the selected date is equal to or greater than the current date
+      if (selectedDate < currentDate) {
+        throw new Error("Please select a date equal to or greater than the current date.");
+      }
+
+      const response = await fetch (apiUrl,{
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          name: addPartyForm.name.value,
-          date: addPartyForm.date.value,
-          time: addPartyForm.time.value,
-          location: addPartyForm.location.value,
-          description: addPartyForm.description.value,
+          name: addPartyToForm.name.value,
+          date: new Date(addPartyToForm.date.value).toISOString(), // Convert to ISO-8601 format
+          location: addPartyToForm.location.value,
+          description: addPartyToForm.description.value,
         }),
       });
-  
+
       if (!response.ok) {
-        throw new Error("Failed to create party");
+        const errorMessage = await response.text();
+        throw new Error(`Failed to create party. Server response: ${errorMessage}`);
       }
   
       render();
@@ -89,4 +99,31 @@ async function addParty(event) {
       console.error(error);
     }
   }
+// function to delete the data from the server
 
+// Add this inside your addArtistForm event listener block
+
+partyList.addEventListener("click", (event) => {
+
+  if (event.target.classList.contains("delete-party")) {
+      const partyId = event.target.dataset.partyId;
+      deleteParty(partyId);
+  }
+});
+
+async function deleteParty(partyId) {
+  try {
+      const response = await fetch(`${apiUrl}/${partyId}`, {
+          method: "DELETE",
+      });
+
+      if (!response.ok) {
+         const errorMessage = await response.text();
+          throw new Error(`Failed to delete party.Server response: ${errorMessage}`);
+      }
+
+      render();
+  } catch (error) {
+      console.error(error);
+  }
+}
